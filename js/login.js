d6bf0ee919d0091dd75f30d29f925f39da36ebec
Ім'я <bogdan.vey2002@gmail.com>
2018-03-21 23:51:08 +0200

    test

--- js/login.js
+++ js/login.js
@@ -1,3 +1,10 @@
 $(document).ready(function (e) {
 	getAll();
+	function Register() {
+    var email = document.getElementById("E-mail");
+	var login = document.getElementById("name");
+	var password = document.getElementById("password");
+	postnew(email,login,password)
+}
+
 });
