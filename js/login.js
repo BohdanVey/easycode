63aeaeacc8ceb18d9672d89f90724b34af5ec197
Ім'я <bogdan.vey2002@gmail.com>
2018-04-17 21:21:54 +0300

    js

--- js/login.js
+++
@@ -1,7 +0,0 @@
-function registerUser() {
-    var email = document.getElementById("email").value;
-	var login = document.getElementById("name").value;
-	var password = document.getElementById("password").value;
-	console.log(email);
-	post(email, login, password);
-};
