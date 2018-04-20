734e3b3b3bb1725fe6fba57c11cd3086f271db7f
Ім'я <bogdan.vey2002@gmail.com>
2018-04-20 18:08:31 +0300

    algo3

---
+++ js/algo3.js
@@ -0,0 +1,28 @@
+function checkAnswers() {
+    let answers = ["a", "b", "b", "1", "40";
+    let forms = [
+        document.getElementById("q1"),
+        document.getElementById("q2"),
+        document.getElementById("q3"),
+        document.getElementById("q4"),
+        document.getElementById("q5"),];
+    for(let i = 0; i < forms.length; i++){
+        if(forms[i].value != answers[i]){
+            // Wrong
+            $("#correct-alert").hide();
+            $("#wrong-alert").show();
+            $('html,body').animate({scrollTop: document.body.scrollHeight},"slow");
+            return;
+        }
+    }
+    // Correct
+    $("#wrong-alert").hide();
+    $("#correct-alert").show();
+    $('html,body').animate({scrollTop: document.body.scrollHeight},"slow");
+    
+    let item = "item_three";
+    put(item, user._id);
+    user[item] = true;
+    $("#algo3").addClass("done");
+    $("#algo3_load").show();
+}
